833f614f201768976f80542d7267479b2f836543
jinyuanzhen <jinyuanzhen@shanghai3h.com>
2018-05-15 18:01:15 +0800

    搭载swagger 增加测试接口

--- service/WechatService.cs
+++ service/WechatService.cs
@@ -54,8 +54,9 @@ namespace service
         }
 
         /// <summary>
-        /// 获取企业号token
+        /// 发送消息给个人(id,text写死测试)
         /// </summary>
+        /// <param name="token">企业号Token</param>
         /// <returns></returns>
         public string send(string token)
         {
@@ -63,17 +64,18 @@ namespace service
             Text text = new Text();
             text.content = "欢迎登陆<a href=\"http://i199527q33.imwork.net:8000\">自家用测试版</a>！\n后续功能将逐步开放，敬请期待！";
             sendToUserModel stum = new sendToUserModel();
-            stum.touser = "JinYuanZhen";
-            stum.msgtype = "text";
-            stum.agentid = 1000002;
+            stum.touser = "JinYuanZhen";//发送对象的微信id
+            stum.msgtype = "text";//发送信息类型
+            stum.agentid = 1000002;//企业应用id
             stum.text = text;
-            stum.safe = 0;
+            stum.safe = 0;//是否保密 0--否 1--是
             var jsontest = Newtonsoft.Json.JsonConvert.SerializeObject(stum);
             var strtest = jsontest.ToString();
+            
             string url = string.Format("https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={0}", token);
 
+            #region 发送参数的处理
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-
             request.Method = "POST";
             request.ContentType = "text/html;charset=UTF-8";
             request.UserAgent = null;
@@ -84,7 +86,6 @@ namespace service
             {
                 reqStream.Write(postBytes, 0, postBytes.Length);
             }
-
             //HttpContext.Current.Response.Write(Newtonsoft.Json.JsonConvert.SerializeObject(stum));
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             string encoding = response.ContentEncoding;
@@ -95,8 +96,36 @@ namespace service
             StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(encoding));
             string retString = reader.ReadToEnd();
             reader.Close();
-
-            return null;
+            #endregion
+            return retString;
         }
+
+        /// <summary>
+        /// 通过ID获取企业成员
+        /// </summary>
+        /// <returns></returns>
+        public string getQYMemberById(string token,string id)
+        {
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");//处理跨域问题
+            string url = string.Format("https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token={0}&userid={1}", token, id);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+            request.UserAgent = null;
+            request.Timeout = 10000;
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            string resultString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+            //JObject resultJSON = (JObject)JsonConvert.DeserializeObject(resultString);
+            return resultString.ToString() ;
+        }
+
     }
+
+
 }
---
+++ weixinApi/Controllers/QYwechatController.cs
@@ -0,0 +1,75 @@
+﻿using service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace weixinApi.Controllers
+{
+    public class QYwechatController : ApiController
+    {
+        /// <summary>
+        /// 发送消息给个人(id,text写死测试)
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("wechatApi/QYwechat/sendMessageTest")]
+        [ActionName("sendMessageTest")]
+        public string sendMessageTest()
+        {
+            try {
+                WechatService s = new WechatService();
+                var QYToken = s.getQYToken();
+                var result=s.send(QYToken);
+                return result;
+            }
+            catch (Exception e) {
+                return e.ToString();
+            }
+        }
+        /// <summary>
+        /// 通过ID获取企业成员
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("wechatApi/QYwechat/getQYMemberById")]
+        [ActionName("getQYMemberById")]
+        public string getQYMemberById(string id)
+        {
+            try
+            {
+                WechatService s = new WechatService();
+                var QYToken = s.getQYToken();
+                var result = s.getQYMemberById(QYToken,id);
+                return result;
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+
+        // GET: api/wechat/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/wechat
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/wechat/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/wechat/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
